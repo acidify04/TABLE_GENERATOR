4cee416b63b91e8b9c5f6df8f54fd83c487abe2d
hyenwon <vn72045470@gmail.com>
2024-12-04 21:55:43 +0900

    console 수정

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,15 @@
 #include "course_db.h"
+#include "display.h"
+//#include "schedule_management.h"
+#include "table_db.h"
+#include <conio.h>
 #include <iostream>
 #include <string>
-#include "table_db.h"
 
 using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
-    TableDatabase db;
-    Table table("<table><id>101</id><name>Park</name><user_id>20231234</user_id><year>2</year><Semester>Fall</Semester><course><id>10000</id><name>OOP</name><professor>Kim</professor><type>10</type><semester>2</semester><english>1</english><professor>string</professor><department>integer</department><department>integer</department><time><weekday>integer</weekday><t>3</t><room>727</room><building>310</building></time><time><weekday>4</weekday><t>4</t><room>728</room><building>310</building></time></course></table>");
-    db.insert(table);
-    db.save();
-    db.load();
-    
+    mainMenu();
     return 0;
 }
